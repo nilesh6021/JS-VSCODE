964e6007b6b960a9484c046149cba445361565b9
nilesh6021 <144022325+nilesh6021@users.noreply.github.com>
2023-12-19 12:28:24 +0000

    DATASAVE

--- Variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 10001
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-
-//node/Basics/Variables.js
-//account_Id =2
-console.log(account_Id);
